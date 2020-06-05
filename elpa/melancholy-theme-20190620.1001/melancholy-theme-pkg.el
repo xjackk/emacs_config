a8f9467cf7b3a823c1ab96e11a1a1a95dd6a2c62
xjackk <xkillilea@gmail.com>
2020-06-04 20:18:11 -0400

    update

--- elpa/melancholy-theme-20190620.1001/melancholy-theme-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "melancholy-theme" "20190620.1001" "No description available." 'nil :commit "3140860d0b310b6ff51b0df11de992cd65135692")
--- elpa/tern-20181108.722/tern-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "tern" "20181108.722" "Tern-powered JavaScript integration" '((json "1.2") (cl-lib "0.5") (emacs "24")) :commit "a295a80a502771cc07dc061961e0eb85343c2925" :authors '(("Marijn Haverbeke")) :maintainer '("Marijn Haverbeke") :url "http://ternjs.net/")
